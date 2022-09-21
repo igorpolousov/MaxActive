d185c49cdb73bc2f500d27571e2d5a9241384fcf
Igor Polousov <igorpolousov@gmail.com>
2022-09-21 13:54:02 +0300

    Initial Commit

---
+++ MaxActiveUITests/MaxActiveUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MaxActiveUITestsLaunchTests.swift
+//  MaxActiveUITests
+//
+//  Created by Igor Polousov on 21.09.2022.
+//
+
+import XCTest
+
+final class MaxActiveUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
